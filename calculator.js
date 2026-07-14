96072443fe8dc5183484b4804262946216a67969
cycling-lion-cub <omglion2club@gmail.com>
2026-07-14 11:00:59 +0900

    Add new arrow function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,22 @@
-function add(a, b) {
+// 더하기 함수
+const add = (a, b) => {
   return a + b;
-}
+};
 
-function substract(a, b) {
+// 빼기 함수
+const substract = (a, b) => {
   return a - b;
-}
+};
+
+// 곱하기 함수
+const multiply = (a, b) => {
+  return a * b;
+};
+
+// 나누기 함수
+const divide = (a, b) => {
+  if (b === 0) {
+    throw new Error();
+  }
+  return a / b;
+};
