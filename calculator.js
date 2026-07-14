1ae578e3090a062ed8b559c3ff933a04f8fa55fc
cycling-lion-cub <omglion2club@gmail.com>
2026-07-14 11:24:35 +0900

    modify comments

--- calculator.js
+++ calculator.js
@@ -1,22 +1,22 @@
-// 더하기 함수
+// 덧셈
 const add = (a, b) => {
   return a + b;
 };
 
-// 빼기 함수
+// 뺄셈
 const substract = (a, b) => {
   return a - b;
 };
 
-// 곱하기 함수
+// 곱셈
 const multiply = (a, b) => {
   return a * b;
 };
 
-// 나누기 함수
+// 나눗셈
 const divide = (a, b) => {
   if (b === 0) {
-    // 분모가 0일때는 나누기 함수가 작동하는걸 차단
+    // 분모가 0일때는 나누기가 진행되지 않도록 차단
     throw new Error();
   }
   return a / b;
